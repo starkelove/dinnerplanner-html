f45b28efc852ed26afd387862659f12f5c9f883e
Anna Eklind <annaekli@kth.se>
2019-09-20 15:27:14 +0200

    listens to numberOfGuests changing

--- src/controller/sidebarController.js
+++ src/controller/sidebarController.js
@@ -2,14 +2,25 @@ class SidebarController {
     constructor(view, model) {
         this.view = view;
         this.model = model;
-
+        
         // TODO lab 3
     }
 
     renderView() {
       this.view.render();
-        // TODO lab 3
+      
+      var self = this;
+      var updateGuests = function() {
+        let num = document.getElementsByClassName("input-num-guests")[0].value;
+        self.model.setNumberOfGuests(num);
+        console.log("hello");
+      }
+
+      document.getElementById("number").addEventListener("input", updateGuests, false);
+      document.getElementById("number").addEventListener("click", updateGuests, false);
+     
     }
 
     // TODO Lab 3
+    
 }
