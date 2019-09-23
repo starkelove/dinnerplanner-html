9db31720bbe4798df699d0aba33b2d5d35bc037d
Anna Eklind <annaekli@kth.se>
2019-09-23 13:05:09 +0200

    add whole html for sideBar

--- src/view/sidebarView.js
+++ src/view/sidebarView.js
@@ -10,11 +10,46 @@ class SidebarView {
   
   render() {
     var content = /* template */ `
-    <div class="container text-center full-vh d-flex align-items-center justify-content-center flex-column">
-      <div class="value-button" id="decrease" onclick="decreaseValue()" value="Decrease Value"></div>
-        <input type="number" class="input-num-guests" id="number" value="1" />
-      <div class="value-button" id="increase" onclick="increaseValue()" value="Increase Value"></div>
+    <div class="row">
+      <div class="col-3">
+          <div id="sideBarView" class="sidebar">
+                  <div class="row">
+                      <div class="col-9">
+                          <h4> My dinner </h4>
+                      </div>
+                  </div>
+                  <div class="row">
+                      <div class="col-4">
+                          <form>
+                              <p class="text"> People </p>
+                              <div class="value-button" id="decrease" onclick="decreaseValue()" value="Decrease Value"></div>
+                                <input type="number" class="input-num-guests" id="number" value="1" />
+                              <div class="value-button" id="increase" onclick="increaseValue()" value="Increase Value"></div>
+                          </form>
+                      </div>
+
+                  </div>
+
+                  <div class="row">
+                      <div class="col-6"> Dish Name</div>
+                      <div class="col-6"> Cost </div>
+                  </div>
+                  <div class="row">
+                      <div class="col-6"><div class="value-main-course-name"></div></div>
+                      <div class="col-6"><div class="value-total-price">0</div> SEK</div>
+                  </div>
+                  <div class="row">
+                      <div class="col-12">
+                          <a id="confirmBtn" class="btn btn-info">
+                              Confirm Dinner
+                          </a>
+                      </div>
+                  </div>
+              </div>
+      </div>
+      <div class="col-9"> </div>
     </div>
+
     `;
     this.container.innerHTML = content;
     this.afterRender();
