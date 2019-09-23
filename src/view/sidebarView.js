6491ba4b70a0e9658b9c1aa32afe5566ce0021b0
Anna Eklind <annaekli@kth.se>
2019-09-23 14:42:38 +0200

    cleaned up

--- src/view/sidebarView.js
+++ src/view/sidebarView.js
@@ -10,46 +10,40 @@ class SidebarView {
 
   render() {
     var content = /* template */ `
-    <div class="row">
-      <div class="col-3">
-          <div id="sidebarView" class="sidebar">
-                  <div class="row">
-                      <div class="col-9">
-                          <h4> My dinner </h4>
-                      </div>
-                  </div>
-                  <div class="row">
-                      <div class="col-4">
-                          <form>
-                              <p class="text"> People </p>
-                              <div class="value-button" id="decrease" onclick="decreaseValue()" value="Decrease Value"></div>
-                                <input type="number" class="input-num-guests" id="number" value="1" />
-                              <div class="value-button" id="increase" onclick="increaseValue()" value="Increase Value"></div>
-                          </form>
-                      </div>
-
-                  </div>
-
-                  <div class="row">
-                      <div class="col-6"> Dish Name</div>
-                      <div class="col-6"> Cost </div>
-                  </div>
-                  <div class="row">
-                      <div class="col-6"><div class="value-main-course-name"></div></div>
-                      <div class="col-6"><div class="value-total-price">0</div> SEK</div>
-                  </div>
-                  <div class="row">
-                      <div class="col-12">
-                          <a id="confirmBtn" class="btn btn-info">
-                              Confirm Dinner
-                          </a>
-                      </div>
+      <div id="sidebarView" class="sidebar">
+              <div class="row">
+                  <div class="col-9">
+                      <h4> My dinner </h4>
                   </div>
               </div>
-      </div>
-      <div class="col-9"> </div>
-    </div>
+              <div class="row">
+                  <div class="col-4">
+                      <form>
+                          <p class="text"> People </p>
+                          <div class="value-button" id="decrease" onclick="decreaseValue()" value="Decrease Value"></div>
+                            <input type="number" class="input-num-guests" id="number" value="1" />
+                          <div class="value-button" id="increase" onclick="increaseValue()" value="Increase Value"></div>
+                      </form>
+                  </div>
 
+              </div>
+
+              <div class="row">
+                  <div class="col-6"> Dish Name</div>
+                  <div class="col-6"> Cost </div>
+              </div>
+              <div class="row">
+                  <div class="col-6"><div class="value-main-course-name"></div></div>
+                  <div class="col-6"><div class="value-total-price">0</div> SEK</div>
+              </div>
+              <div class="row">
+                  <div class="col-12">
+                      <a id="confirmBtn" class="btn btn-info">
+                          Confirm Dinner
+                      </a>
+                  </div>
+              </div>
+          </div>
     `;
     this.container.innerHTML = content;
     this.afterRender();
@@ -66,3 +60,41 @@ class SidebarView {
   }
 
 }
+
+/*const content = `
+  <div id="sidebarView" class="sidebar">
+    <div class="row">
+        <div class="col-9">
+            <h4> My dinner </h4>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <form>
+                <p class="text"> People </p>
+                <div class="value-button" id="decrease" onclick="decreaseValue()" value="Decrease Value"></div>
+                  <input type="number" class="input-num-guests" id="number" value="1" />
+                <div class="value-button" id="increase" onclick="increaseValue()" value="Increase Value"></div>
+            </form>
+        </div>
+
+    </div>
+
+    <div class="row">
+        <div class="col-6"> Dish Name</div>
+        <div class="col-6"> Cost </div>
+    </div>
+    <div class="row">
+        <div class="col-6"><div class="value-main-course-name"></div></div>
+        <div class="col-6"><div class="value-total-price">0</div> SEK</div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <a id="confirmBtn" class="btn btn-info">
+                Confirm Dinner
+            </a>
+        </div>
+    </div>
+  </div>
+`;*/
+
