cf3d83fa83bf4f5238548d6e3c472ca9b7a31548
Anna Eklind <annaekli@kth.se>
2019-09-30 15:55:34 +0200

    does not query document anymore

--- src/controller/overviewController.js
+++ src/controller/overviewController.js
@@ -3,13 +3,10 @@ class OverviewController {
         this.view = view;
         this.model = model;
 
-        // TODO lab 3
     }
 
     renderView() {
       this.view.render();
-
-      var self = this;
 
       var returnToSearch = function(){
         changeMyView("overviewView");
@@ -19,18 +16,11 @@ class OverviewController {
         changeToPrint("overviewView");
       }
 
-      /*
-      let butt = this.view.overviewView.firstChild;
-      console.log(butt);
-      let bitt = (butt.getElementById("returnFromOverview"));
-      bitt.addEventListener("click", returnToSearch, false);*/
-      //this.view.returnFromOverview.addEventListener("click", returnToSearch, false);
-      document.getElementById("returnFromOverview").addEventListener("click", returnToSearch, false);
-      document.getElementById("toPrintBtn").addEventListener("click", goToPrint, false);
+      this.view.returnFromOverview.addEventListener("click", returnToSearch, false);
+      this.view.toPrintBtn.addEventListener("click", goToPrint, false);
 
 
     }
 
-    // TODO Lab 3
 
 }
